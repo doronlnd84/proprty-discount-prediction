6b84bc559aa7c7f53470cf553aa4392e5c7fb003
Doronlnd84 <doronlnd84@gmail.com>
2019-08-16 14:06:12 +0300

    Establish main project of - "property discount prediciton" the project agregets the crowlers and the identification projects

---
+++ property-deal/src/main/java/dl/projects/propertydeal/PropertyDealApplication.java
@@ -0,0 +1,13 @@
+package dl.projects.propertydeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyDealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropertyDealApplication.class, args);
+    }
+
+}
---
+++ property-deal/src/test/java/dl/projects/propertydeal/PropertyDealApplicationTests.java
@@ -0,0 +1,16 @@
+package dl.projects.propertydeal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PropertyDealApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
