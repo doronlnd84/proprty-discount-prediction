4f2adf7ce4d43dc6973fa0aa4c0350af1598d657
Doronlnd84 <doronlnd84@gmail.com>
2019-08-26 10:13:43 +0300

    1. add manipulation data methods both in deal and sale projects 2. add new empty project - sale-deal-identity

---
+++ sale-deal-identity/src/main/java/dl/projects/propertydiscountprediction/saledealidentity/SaleDealIdentityApplication.java
@@ -0,0 +1,13 @@
+package dl.projects.propertydiscountprediction.saledealidentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaleDealIdentityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaleDealIdentityApplication.class, args);
+    }
+
+}
---
+++ sale-deal-identity/src/test/java/dl/projects/propertydiscountprediction/saledealidentity/SaleDealIdentityApplicationTests.java
@@ -0,0 +1,16 @@
+package dl.projects.propertydiscountprediction.saledealidentity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SaleDealIdentityApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
